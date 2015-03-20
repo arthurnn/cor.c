fb2a1224b93f1839d0d38e53892b1d9683b7873c
Arthur Neves <arthurnn@gmail.com>
2015-03-20 18:12:16 -0400

    First commit

---
+++ cor.c
@@ -0,0 +1,13 @@
+#include <cor.h>
+
+void start(coroutine* c, func f, void* arg, void* sp)
+{
+}
+
+void resume(coroutine *c)
+{
+}
+
+void yield(coroutine* c)
+{
+}
---
+++ cor.h
@@ -0,0 +1,12 @@
+#include <setjmp.h>
+
+typedef struct {
+  jmp_buf callee_ctx;
+  jmp_buf caller_ctx;
+} coroutine;
+
+typedef void (*func)(void*);
+
+void start(coroutine* c, func f, void* arg, void* sp);
+void resume(coroutine *c);
+void yield(coroutine* c);
