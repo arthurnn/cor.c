3633374de1113cd6a8a5fc1fc0ddfbaf94ad41e8
Arthur Neves <arthurnn@gmail.com>
2015-03-20 23:24:29 -0400

    Add next and yield methods

--- cor.c
+++ cor.c
@@ -7,11 +7,18 @@ void start(coroutine* c, func f, void* arg, void* sp)
 
 int next(coroutine* c)
 {
-  // jumtp to callee context
-  return 0;
+  int ret = setjmp(c->caller_ctx);
+  if(!ret) {
+    longjmp(c->callee_ctx, 1);
+  }
+  else {
+    return ret == 1;
+  }
 }
 
 void yield(coroutine* c)
 {
-  // jump to caller context
+  if(!setjmp(c->callee_ctx)) {
+    longjmp(c->caller_ctx, 1);
+  }
 }
